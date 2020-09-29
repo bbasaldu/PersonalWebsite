435e25e70db4e5e5cf208183bf4d37f9c51a43c6
bbasaldu <50085901+bbasaldu@users.noreply.github.com>
2020-09-29 13:55:34 -0700

    Add files via upload

--- d3DOM.js
+++ d3DOM.js
@@ -3,10 +3,11 @@ var ul = d3.select('ul');
 var div = d3.select('div');
 
 ul.append('li').append('text').text("Zoomable Bar Chart").attr("link", "https://bbasaldu.github.io/CannibasBarChart/");
-ul.append('li').append('text').text("Pan/drag/zoom Scatter Plot").attr("link", "https://bbasaldu.github.io/ScatterPlot/");
-ul.append('li').append('text').text("On/Off Multi Line Chart").attr("link", "https://bbasaldu.github.io/MultiLine-Chart/");
-ul.append('li').append('text').text("Bubble Pack Layout").attr("link", "https://bbasaldu.github.io/MetaCritic-Review-Visualizer/");
+ul.append('li').append('text').text("Best Overwatch League Bar Chart").attr("link", "https://bbasaldu.github.io/OverwatchLeagueDataVisualization/");
 ul.append('li').append('text').text("Census Data Geo Map").attr("link", "https://bbasaldu.github.io/CensusDataGeoMap/");
+ul.append('li').append('text').text("On/Off Multi Line Chart").attr("link", "https://bbasaldu.github.io/MultiLine-Chart/");
+ul.append('li').append('text').text("Pan/drag/zoom Scatter Plot").attr("link", "https://bbasaldu.github.io/ScatterPlot/");
+ul.append('li').append('text').text("Bubble Pack Layout").attr("link", "https://bbasaldu.github.io/MetaCritic-Review-Visualizer/");
 ul.append('li').append('text').text("Census Data Grouped Bar Chart").attr("link", "https://bbasaldu.github.io/CensusDataGroupedBarChart/");
 //ul.append('li').text("3 Chart");//placeholders for future projects
 //ul.append('li').text("4 Chart");
