fb7e4c919c0ef4a2878dd80dd5018e0f3a3dcb5a
bbasaldu <50085901+bbasaldu@users.noreply.github.com>
2021-04-02 22:02:38 -0700

    Add files via upload

--- d3DOM.js
+++ d3DOM.js
@@ -1,6 +1,8 @@
-var body = d3.select('body');
-var ul = d3.select('ul');
-var div = d3.select('div');
+let body = d3.select('body');
+let ul = d3.select('ul');
+let div = d3.select('div');
+let svg = d3.select('#mainSvg')
+let title = d3.selectAll('.title')
 
 ul.append('li').append('text').text("Zoomable Bar Chart").attr("link", "https://bbasaldu.github.io/CannibasBarChart/");
 ul.append('li').append('text').text("Best Overwatch League Bar Chart").attr("link", "https://bbasaldu.github.io/OverwatchLeagueDataVisualization/");
@@ -11,62 +13,52 @@ ul.append('li').append('text').text("On/Off Multi Line Chart").attr("link", "htt
 ul.append('li').append('text').text("Pan/drag/zoom Scatter Plot").attr("link", "https://bbasaldu.github.io/ScatterPlot/");
 ul.append('li').append('text').text("Bubble Pack Layout").attr("link", "https://bbasaldu.github.io/MetaCritic-Review-Visualizer/");
 ul.append('li').append('text').text("Census Data Grouped Bar Chart").attr("link", "https://bbasaldu.github.io/CensusDataGroupedBarChart/");
-//ul.append('li').text("3 Chart");//placeholders for future projects
-//ul.append('li').text("4 Chart");
-//ul.append('li').text("5 Chart");
-
-var list = d3.selectAll('li');
+let list = d3.selectAll('li');
 list.attr("class", "list");
+let bottomText = ul.selectAll('text');
+bottomText.attr('class', 'text');
+let listEase = d3.easeBounceOut;
+let textEase = d3.easeQuadIn;
+let topText = d3.select('.top').selectAll('text')
+let info = d3.select('#info')
 
-ul.selectAll('text').style('pointer-events', 'auto');
+let line = svg.append('line')
+  .attr('x1', '0%')
+  .attr('y1', '0%')
+  .attr('x2', '0%')
+  .attr('y2', '0%')
+  .attr('stroke', '#000')
+  .attr('stroke-width', 3)
+line.transition().duration(1500).ease(listEase)
+  .attr('x2', '100%')
 
-var viewBox = ul.node().getBoundingClientRect();
 
-let y = 0;
-ul.on("wheel", function(){
-    //console.log(event)
-    var firstElem = ul.node().firstElementChild.getBoundingClientRect();
-    var topLi = ul.node().firstElementChild;
-    var lastElem = ul.node().lastElementChild.getBoundingClientRect();
-    var BottomLi = ul.node().lastElementChild;
-    
-    event.preventDefault();
-    var dy;
-    //to limit scroll speed
-    if(event.deltaY > 100) dy = 100
-    else if(event.deltaY < -100) dy = -100;
-    else dy = event.deltaY;
-    y += dy;
-    //if our future bbox value will exceed the viewbox value then trigger
-    if((firstElem.bottom) < viewBox.top){
-        //console.log('tigger1')
-        y = y + firstElem.height;
-        var clone = topLi.cloneNode(true);
-        clone.style.top = y;
-        ul.node().appendChild(clone);
-        topLi.remove();
-    }
-    else if(((firstElem.top+y) > viewBox.top)){
-        //console.log('trigger2')
-        y = y - lastElem.height;
-        var clone = BottomLi.cloneNode(true);
-        clone.style.top = y;
-        ul.node().prepend(clone);
-        BottomLi.remove();
-    }
-    ul = d3.select('ul');
-    list = d3.selectAll('li'); //new appended child might not be in 'list' so redefine 'list'
-    list.style("top", y+"px");
-    list.on("click", listClick);
-    
-    //maybe have a way of resetting y, since y technically can take up infinite memory if i keep increasing it.
-});
+title
+  .style('left', '-100%')
+list
+  .style('left', (d,i) => (i%2 == 0)?'-100%':'100%')
+topText
+  .style('opacity', 0)
+  .transition().duration(1000).ease(textEase)
+  .style('opacity', 1)
+bottomText
+  .style('opacity', 0)
+  .transition().duration(1000).ease(textEase)
+  .style('opacity', 1)
+info
+  .style('opacity', 0)
+  .transition().duration(1500).ease(textEase)
+  .style('opacity', 1)
 
-list.on("click", listClick);    
+title.transition().duration(1500).ease(listEase)
+  .style('left', (d,i) => (i==0)?'5%':'10%')
+list.transition().duration(1500).ease(listEase)
+  .style('left', '0%')
 
-function listClick(){
-    var txt = d3.select(this).select('text');
-    window.location = txt.attr("link");
+list.on('click', function(){
+  openLink(d3.select(this).select('text').attr('link'))
+})
+
+function openLink(link){
+  window.open(link, '_blank')
 }
-
-
